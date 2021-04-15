5bcb28d904d52abeeb485c43ac069a53357eb2e2
Gergely Tanczos <gergelytanczos2000@gmail.com>
2021-04-15 11:21:18 +0200

    controllers + kerdesek.html + models + .gitignore modositas

---
+++ HajosTeszt/HajosTeszt/Controllers/BoatController.cs
@@ -0,0 +1,24 @@
+﻿using HajosTeszt.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controllers
+{
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult M1()
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdések = from i in context.Questions
+                           select i.QuestionText;
+
+            return new JsonResult(kérdések);
+        }
+    }
+}
